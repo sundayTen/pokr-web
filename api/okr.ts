19185befc83dd1f31070fcdbd9a04172102d43aa
hojunin <dlsghwns@naver.com>
2023-02-03 00:20:59 +0900

    feat: 리액트쿼리-SSR 구현(initialData 활용)

---
+++ api/okr.ts
@@ -0,0 +1,17 @@
+export const fetchOkr = async () => {
+  const res = await fetch('http://localhost:8080/api/me/okr', {
+    method: 'GET',
+    cache: 'default', // SSR 타입에 따라 분기
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6bnVsbCwibmlja25hbWUiOm51bGwsImVtYWlsIjpudWxsLCJleHAiOjE3MDY4ODQ4ODF9.EihO9VK3rte6esD_hOTQfp3be1Qi9UZ0gpjsud1-tgY',
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error('데이터 페칭 실패');
+  }
+
+  return res.json();
+};
