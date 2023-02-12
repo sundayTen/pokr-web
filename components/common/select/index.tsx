84c0cb4cdee7a432bc094275463ceae71040719d
phrygia <dmsgp62@gmail.com>
2023-02-12 20:41:31 +0900

    pc tooltip - close #28

--- components/common/select/index.tsx
+++ components/common/select/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 import styles from '@components/common/select/Select.module.scss';
 
