404ee0bc10df2cafa2824acd4d0923c84cb18804
phrygia <dmsgp62@gmail.com>
2023-03-03 12:23:21 +0900

    [ADD] bottom sheet 컴포넌트 추가 #27

---
+++ components/common/bottomSheep/index.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import cn from 'classnames';
+import styles from '@components/common/bottomSheep/BottomSheep.module.scss';
+import AutoHeightImage from '../image';
+
+interface BottomSheepProps {
+  children: React.ReactNode;
+  title?: string;
+  subTitle?: string;
+  open: boolean;
+  onClose: () => void;
+}
+
+const BottomSheep = ({
+  children,
+  title,
+  subTitle,
+  open,
+  onClose,
+}: BottomSheepProps) => {
+  const [animate, setAnimate] = useState<boolean>(false);
+  const [visible, setVisible] = useState<boolean>(open);
+
+  useEffect(() => {
+    setVisible(open);
+
+    if (visible && !open) {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 200);
+    }
+    return () => {
+      setVisible(false);
+    };
+  }, [visible, open]);
+
+  if (!animate && !visible) return null;
+
+  return (
+    <div
+      onClick={onClose}
+      className={cn(styles.root, {
+        [styles.open]: open,
+      })}
+    >
+      <div className={styles.background} />
+      <div className={cn(styles.container)}>
+        <div className={styles.header}>
+          {title && <h2>{title}</h2>}
+          {subTitle && <p>{subTitle}</p>}
+          <button type="button" className={styles.closeBtn}>
+            <AutoHeightImage
+              src="/images/close.png"
+              alt="검색"
+              width={18}
+              height={18}
+            />
+          </button>
+        </div>
+        <div className={styles.inner}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default BottomSheep;
