4c2df890c1ba28909e27f43609834ca631045147
hojunin <dlsghwns@naver.com>
2023-03-05 19:25:37 +0900

    feat: fetcher 모듈 추가
    feat: 표현 수정

--- api/okr.ts
+++ api/okr.ts
@@ -1,26 +1,13 @@
 import { OKR as OKR_TYPE } from '@type/okr';
+import { fetcher } from './fetcher';
+import { OKR } from './path';
 
 export const fetchOkr = async (): Promise<OKR_TYPE[]> => {
-  const res = await fetch(`http://127.0.0.1:8080/api/me_api/okr`, {
-    method: 'GET',
-    cache: 'default', // SSR 타입에 따라 분기
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6bnVsbCwibmlja25hbWUiOm51bGwsImVtYWlsIjpudWxsLCJleHAiOjE3MDkwMjE5NjN9.BB5PZHeCu3Lp41E1l_I059Z7HmK7rwDh57iVHeRARag',
-    },
-    mode: 'cors',
-  });
-
-  if (!res.ok) {
-    switch (res.status) {
-      case 401:
-        throw new Error('인증 문제 발생');
-
-      default:
-        throw new Error('데이터 페칭 실패');
-    }
+  try {
+    const res = await fetcher<OKR_TYPE[]>({ path: OKR });
+    return res;
+  } catch (error) {
+    throw error;
+    // 에러 핸들링
   }
-
-  return res.json();
 };
