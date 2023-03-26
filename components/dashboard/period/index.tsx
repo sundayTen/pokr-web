27dc128ac6e6ce2959001efb5a0727f0b294dfbc
phrygia <dmsgp62@gmail.com>
2023-03-26 18:38:24 +0900

    대시보드 okr 평균치 구현 -close #33

--- components/dashboard/period/index.tsx
+++ components/dashboard/period/index.tsx
@@ -6,10 +6,10 @@ import styles from '@components/dashboard/period/DashBoardPeriod.module.scss';
 import useMountEffect from '@hooks/useMountEffect';
 import { useQuery } from '@tanstack/react-query';
 import { METRICS_HALF } from '@api/path';
-import { fetchMetrics } from '@api/metrics';
+import { fetchMetrics, fetchMetricsObjects } from '@api/metrics';
 import useIsMobile from '@hooks/useIsMobile';
 import dashBoardStore, { GRAPH_DATA } from '@store/dashboard';
-import { METRICS } from '@type/metrics';
+import { METRICS, METRICS_OBJECTIVES_DATA } from '@type/metrics';
 
 const periodArr = ['반기', '분기'];
 const halfPeriodArr = ['상반기', '하반기'];
@@ -20,11 +20,14 @@ const DashBoardPeriod = () => {
   const [tooltipOpen, setTooltipOpen] = useState<boolean>(false);
   const [period, setPeriod] = useState<string>(periodArr[0]);
   const [categorize, setCategorize] = useState<string>(halfPeriodArr[0]);
+  const [obejectStatus, setObejectStatus] = useState(false); // true: 달성완료? false: 달성중
+  const [keyResultPercent, setKeyResultPercent] = useState(0);
+  const [initiativePercent, setInitiativePercent] = useState(0);
   const { changeGraphData } = dashBoardStore();
 
   let metricsNumber = 1;
 
-  const { isError, isLoading, refetch } = useQuery<METRICS[]>(
+  const { refetch } = useQuery<METRICS[]>(
     [`${period === '반기' ? 'metricsHalf' : 'metricsQuarter'}`],
     () => fetchMetrics(metricsNumber, period === '반기' ? 'half' : 'quarter'),
     {
@@ -48,6 +51,42 @@ const DashBoardPeriod = () => {
     },
   );
 
+  const { isLoading } = useQuery<METRICS_OBJECTIVES_DATA[]>(
+    ['objectives'],
+    fetchMetricsObjects,
+    {
+      suspense: true,
+      useErrorBoundary: true,
+      initialData: [],
+      onSuccess: (data: METRICS_OBJECTIVES_DATA[]) => {
+        const objectivesData = data.reduce(
+          (acc, cur: METRICS_OBJECTIVES_DATA) => {
+            return {
+              ...acc,
+              keyResult: acc.keyResult + cur.keyResultPercent,
+              initiative: acc.initiative + cur.initiativePercent,
+              achievement: cur.achievement ? true : false,
+            };
+          },
+          { keyResult: 0, initiative: 0, achievement: false },
+        );
+
+        if (objectivesData.keyResult > 0) {
+          setKeyResultPercent(
+            Math.floor(objectivesData.keyResult / data.length),
+          );
+        }
+        if (objectivesData.initiative > 0) {
+          setInitiativePercent(
+            Math.floor(objectivesData.initiative / data.length),
+          );
+        }
+
+        setObejectStatus(objectivesData.achievement);
+      },
+    },
+  );
+
   useMountEffect(() => {
     setCategorize(period === '반기' ? halfPeriodArr[0] : quarterPeriodArr[0]);
     refetch();
@@ -55,9 +94,26 @@ const DashBoardPeriod = () => {
     return () => {};
   }, [period]);
 
-  // if (isError || isLoading) {
-  //   return <></>;
-  // }
+  const [leftTimes, setLeftTimes] = useState<string>();
+  const deadline = `December, 31, ${new Date().getFullYear()}`;
+
+  const getTime = () => {
+    const time = Date.parse(deadline) - Date.now();
+
+    setLeftTimes(
+      `${Math.floor(time / (1000 * 60 * 60 * 24))}일 ${Math.floor(
+        (time / (1000 * 60 * 60)) % 24,
+      )}시간 ${Math.floor((time / 1000 / 60) % 60)}분 남았어요`,
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(getTime, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // console.log(leftDays);
 
   return (
     <div className={styles.root}>
@@ -102,59 +158,65 @@ const DashBoardPeriod = () => {
                 height={20}
               />
               <ToolTip
-                text="목표 기간 : 2023.01.01 ~ 2023.12.31"
+                text={`목표 기간 : ${new Date().getFullYear()}.01.01 ~ ${new Date().getFullYear()}.12.31`}
                 open={tooltipOpen}
                 clasName={styles.tooltip}
               />
             </button>
-            148일 15시간 45분 남았어요
+            {leftTimes}
           </div>
         )}
       </div>
       <ul className={styles.categoryContainer}>
         <li className={styles.objectives}>
-          <AutoHeightImage
-            src="/images/objectives.png"
-            alt="목표"
-            width={48}
-            height={48}
-          />
+          <div className={styles.icon}>
+            <AutoHeightImage
+              src="/images/trophy.png"
+              alt="목표"
+              width={24}
+              height={24}
+            />
+          </div>
           <div>
-            <strong>34%</strong>
-            <p>Objectives</p>
+            <strong>{obejectStatus ? '달성완료' : '달성중'}</strong>
+            <p>목표</p>
           </div>
           <div className={styles.progress}>
-            <span style={{ width: '34%' }} />
+            <span style={{ width: `${obejectStatus ? 100 : 0}%` }} />
           </div>
         </li>
         <li className={styles.keyResult}>
-          <AutoHeightImage
-            src="/images/key-result.png"
-            alt="결과"
-            width={48}
-            height={48}
-          />
+          <div className={styles.icon}>
+            <AutoHeightImage
+              src="/images/flag.png"
+              alt="결과"
+              width={24}
+              height={24}
+            />
+          </div>
           <div>
-            <strong>61%</strong>
-            <p>Key Results</p>
+            <strong>{keyResultPercent}%</strong>
+            <p>핵심 지표</p>
           </div>
           <div className={styles.progress}>
-            <span style={{ width: '61%' }} />
+            <span style={{ width: `${keyResultPercent}%` }} />
           </div>
         </li>
         <li className={styles.initiatives}>
-          <AutoHeightImage
-            src="/images/initiatives.png"
-            alt="계획"
-            width={48}
-            height={48}
-          />
+          <div className={styles.icon}>
+            <AutoHeightImage
+              src="/images/todo.png"
+              alt="계획"
+              width={24}
+              height={24}
+            />
+          </div>
           <div>
-            <strong>82%</strong>
-            <p>Initiatives</p>
+            <strong>{initiativePercent}%</strong>
+            <p>주요 행동</p>
           </div>
           <div className={styles.progress}>
-            <span style={{ width: '82%' }} />
+            <span style={{ width: `${initiativePercent}%` }} />
           </div>
         </li>
       </ul>
