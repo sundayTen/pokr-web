6d25998f069cb3a8832718533e28407db2b1ebe3
phrygia <dmsgp62@gmail.com>
2023-04-29 16:56:45 +0900

    충돌수정

--- components/goal-management/goalCard/goalCardItem/index.tsx
+++ components/goal-management/goalCard/goalCardItem/index.tsx
@@ -1,22 +1,102 @@
+import AutoHeightImage from '@components/common/image';
 import { OKY_KEY_RESULT_TYPE } from '@type/okr';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import cn from 'classnames';
 import styles from './goalCardItem.module.scss';
 
 const GoalCardItem = ({ data }: { data: OKY_KEY_RESULT_TYPE }) => {
+  const [title, setTitle] = useState(data.title);
+  const [disabledTitle, setDisabledTitle] = useState(true);
+  const [showMore, setShowMore] = useState(false);
+  const titleRef = useRef() as React.MutableRefObject<HTMLTextAreaElement>;
+  const btnRef = useRef() as React.MutableRefObject<HTMLButtonElement>;
   // console.log(data);
   // fetchOkrObjectives
+
+  useEffect(() => {
+    const handleCloseSelect = (e: React.BaseSyntheticEvent | MouseEvent) => {
+      if (showMore && (!btnRef.current || !btnRef.current.contains(e.target))) {
+        setShowMore(false);
+      }
+    };
+
+    window.addEventListener('click', handleCloseSelect);
+
+    return () => {
+      window.removeEventListener('click', handleCloseSelect);
+    };
+  }, [showMore]);
 
   return (
     <div className={styles.root}>
       <div className={styles.top}>
         <span className={styles.tag}>달성중</span>
-        <button type="button">write</button>
-        <button type="button">more</button>
+        <button
+          type="button"
+          className={styles.editButton}
+          onClick={() => {
+            setDisabledTitle(false);
+            titleRef.current.focus();
+          }}
+        >
+          <AutoHeightImage
+            src="/images/card/edit.png"
+            alt=""
+            width="24"
+            height="24"
+          />
+        </button>
+        <button
+          type="button"
+          className={styles.moreButton}
+          onClick={() => setShowMore(!showMore)}
+          ref={btnRef}
+        >
+          <AutoHeightImage
+            src={`/images/more/more_${showMore ? 'checked' : 'unchecked'}.png`}
+            alt=""
+            width="24"
+            height="24"
+          />
+        </button>
+        {showMore && (
+          <div className={styles.moreInner}>
+            <button type="button">삭제</button>
+          </div>
+        )}
       </div>
-      <h2>{data.title}</h2>
+      <textarea
+        ref={titleRef}
+        className={styles.title}
+        value={title}
+        // onKeyPress={(e) => {
+        //   if (e.key === 'Enter') {
+        //     setDisabledTitle(true);
+        //   }
+        // }}
+        rows={2}
+        onChange={(e) => {
+          const str = e.target.value?.split('\n');
+          if (str?.length <= 2) {
+            setTitle(e.target.value);
+          }
+          // setTitle(true);
+        }}
+        onKeyDown={(e) => {
+          let numberOfLines =
+            ((e.target as HTMLInputElement).value.match(/\n/g) || []).length +
+            1;
+
+          if (e.which === 13 && numberOfLines >= 2) {
+            return false;
+          }
+        }}
+        disabled={disabledTitle}
+      />
       <p>
         {data.openDate} ~ {data.dueDate}
       </p>
+      <div className={styles.bottom}></div>
     </div>
   );
 };
--- components/goal-management/goalCard/index.tsx
+++ components/goal-management/goalCard/index.tsx
@@ -1,17 +1,20 @@
 import { fetchOkrObjectives } from '@api/objectives';
+import useMountEffect from '@hooks/useMountEffect';
 import goalManagementStore from '@store/goal-management';
 import { useQuery } from '@tanstack/react-query';
 import { OKY_KEY_RESULT_TYPE } from '@type/okr';
-import React, { useEffect } from 'react';
-import Slider from 'react-slick';
+import React, { useEffect, useRef, useState } from 'react';
+// import Slider from 'react-slick';
 import GoalCardItem from './goalCardItem';
 import styles from './GoalCardList.module.scss';
 
 // https://react-slick.neostack.com/docs/example/multiple-items
 const GoalCardList = ({ cards }: { cards: any }) => {
+  // const [nav1, setNav1] = useState<Slider | null | undefined>();
+  // const [nav2, setNav2] = useState<Slider | null | undefined>();
   const { currentYear } = goalManagementStore();
   const { data, refetch } = useQuery(
-    ['okr'],
+    ['okr_objectives'],
     () => fetchOkrObjectives(Number(currentYear)),
     {
       suspense: true,
@@ -20,31 +23,59 @@ const GoalCardList = ({ cards }: { cards: any }) => {
     },
   );
 
-  useEffect(() => {
+  useMountEffect(() => {
     refetch();
   }, [currentYear]);
 
-  console.log(cards);
+  console.log(cards, data);
 
-  const settings = {
-    className: 'slider variable-width',
-    dots: false,
-    infinite: false,
-    centerMode: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-  };
+  // const settings = {
+  //   className: 'slider variable-width',
+  //   dots: false,
+  //   infinite: false,
+  //   centerMode: false,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   variableWidth: true,
+  // };
+
+  // const settings = {
+  //   customPaging: function (i) {
+  //     return <div>{i}</div>;
+  //   },
+  //   dots: true,
+  //   dotsClass: 'slick-dots slick-thumb',
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  // };
 
   return (
     <div className={styles.root}>
-      {cards?.length > 0 && (
-        <Slider {...settings}>
-          {cards.map((card: OKY_KEY_RESULT_TYPE) => (
-            <GoalCardItem key={card.id} data={card} />
-          ))}
-        </Slider>
-      )}
+      {/* <Slider
+        asNavFor={nav1}
+        ref={(slider2) => setNav2(slider2)}
+        // slidesToShow={3}
+        variableWidth={true}
+        infinite={false}
+        swipeToSlide={true}
+        focusOnSelect={true}
+        slidesToScroll={1}
+        draggable={false}
+      >
+        {cards.map((card: OKY_KEY_RESULT_TYPE) => (
+          <GoalCardItem key={card.id} data={card} />
+        ))}
+      </Slider>
+      <Slider asNavFor={nav2} ref={(slider1) => setNav1(slider1)}>
+        
+      </Slider> */}
+      <div className={styles.lists}>
+        {cards.map((card: OKY_KEY_RESULT_TYPE) => (
+          <GoalCardItem key={card.id} data={card} />
+        ))}
+      </div>
     </div>
   );
 };
