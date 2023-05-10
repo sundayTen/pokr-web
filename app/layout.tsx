31d8ebcfd99166c22d7516d33e37431ff40e151a
hojunin <dlsghwns@naver.com>
2023-05-10 21:35:53 +0900

    모달 컴포넌트 잠시 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import userStore from '@store/user';
 import GlobalLayout from '@components/common/globalLayout';
-import ModalController from '@components/common/modal/modalController';
+// import ModalController from '@components/common/modal/modalController';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -39,7 +39,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <QueryClientProvider client={queryClient}>
           <ReactQueryDevtools initialIsOpen={false} />
           <GlobalLayout>{children}</GlobalLayout>
-          <ModalController ref={(ref) => ModalController.setRef(ref)} />
+          {/* <ModalController ref={(ref) => ModalController.setRef(ref)} /> */}
           <Analytics />
         </QueryClientProvider>
       </body>
