a15117673ca4fa9c503b197d8bd9b201e2a344f9
hojunin <dlsghwns@naver.com>
2023-05-21 12:29:13 +0900

    fix: 모달기능 원복

--- components/shared/createObjective/index.tsx
+++ components/shared/createObjective/index.tsx
@@ -3,32 +3,42 @@ import styles from './CreateObjective.module.scss';
 import Input from '@components/common/input';
 import useInput from '@hooks/useInput';
 import Select from '@components/common/select';
+import Modal from '@components/common/modal';
 
 const YEARS = ['2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030'];
 
-const CreateObjective = () => {
+const CreateObjective = ({ close }: { close: () => void }) => {
   const [targetYear, setTargetYear] = useState(YEARS[0]);
   const { value, onChangeInput } = useInput({
     type: 'string',
     initialValue: '',
   });
   return (
-    <div className={styles.root}>
-      <Input
-        label="목표명"
-        placeholder="이루고 싶은 목표를 입력해주세요"
-        value={value}
-        onChange={onChangeInput}
-      />
+    <Modal
+      title="목표 추가하기"
+      cancelButtonLabel="취소"
+      confirmButtonLabel="확인"
+      cancelButtonPressed={close}
+      confirmButtonPressed={close}
+      close={close}
+    >
+      <div className={styles.root}>
+        <Input
+          label="목표명"
+          placeholder="이루고 싶은 목표를 입력해주세요"
+          value={value}
+          onChange={onChangeInput}
+        />
 
-      <label className={styles.selectLabel}>목표 연도</label>
+        <label className={styles.selectLabel}>목표 연도</label>
 
-      <Select
-        value={targetYear}
-        options={YEARS}
-        onChange={(e) => setTargetYear(e)}
-      />
-    </div>
+        <Select
+          value={targetYear}
+          options={YEARS}
+          onChange={(e) => setTargetYear(e)}
+        />
+      </div>
+    </Modal>
   );
 };
 
