8ae8fd8a0ac1aabd488aae6189bd1c31e1519918
phrygia <dmsgp62@gmail.com>
2023-06-06 16:57:24 +0900

    모바일 대시보드 기간

--- components/dashboard/period/index.tsx
+++ components/dashboard/period/index.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import cn from 'classnames';
 import AutoHeightImage from '@components/common/autoHeightImage';
 import Select from '@components/common/select';
@@ -17,20 +23,34 @@ const periodArr = ['반기', '분기'];
 const halfPeriodArr = ['상반기', '하반기'];
 const quarterPeriodArr = ['1분기', '2분기', '3분기', '4분기'];
 
+const getOffsetTop = (element: any) => {
+  let newElement = element;
+  let offsetTop = 0;
+  while (newElement) {
+    offsetTop += newElement.offsetTop;
+    newElement = newElement.offsetParent;
+  }
+  return offsetTop;
+};
+
 const DashBoardPeriod = () => {
   const { userToken } = userStore();
   const { isMobile } = useIsMobile();
+  const { changeGraphData } = dashBoardStore();
   const [tooltipOpen, setTooltipOpen] = useState<boolean>(false);
   const [period, setPeriod] = useState<string>(periodArr[0]);
   const [categorize, setCategorize] = useState<string>(halfPeriodArr[0]);
   const [obejectStatus, setObejectStatus] = useState(false); // true: 달성완료? false: 달성중
   const [keyResultPercent, setKeyResultPercent] = useState(0);
   const [initiativePercent, setInitiativePercent] = useState(0);
+  const [leftTimes, setLeftTimes] = useState<string>();
   const [periodModal, setPeriodModal] = useState({
     type: '',
     show: false,
   });
-  const { changeGraphData } = dashBoardStore();
+  const periodRef = useRef<HTMLDivElement | null>(null);
+  // const periodChildRef = useRef<HTMLDivElement | null>(null);
+  const deadline = `December, 31, ${new Date().getFullYear()}`;
 
   let metricsNumber = 1;
 
@@ -93,17 +113,7 @@ const DashBoardPeriod = () => {
     },
   );
 
-  useMountEffect(() => {
-    setCategorize(period === '반기' ? halfPeriodArr[0] : quarterPeriodArr[0]);
-    refetch();
-
-    return () => {};
-  }, [period]);
-
-  const [leftTimes, setLeftTimes] = useState<string>();
-  const deadline = `December, 31, ${new Date().getFullYear()}`;
-
-  const getTime = () => {
+  const getTime = useCallback(() => {
     const time = Date.parse(deadline) - Date.now();
 
     setLeftTimes(
@@ -111,7 +121,28 @@ const DashBoardPeriod = () => {
         (time / (1000 * 60 * 60)) % 24,
       )}시간 ${Math.floor((time / 1000 / 60) % 60)}분 남았어요`,
     );
-  };
+  }, [leftTimes]);
+
+  const handleChangeCategorize = useCallback(
+    (e: string) => {
+      setCategorize(e);
+
+      if (e === '상반기' || e === '1분기') metricsNumber = 1;
+      else if (e === '하반기' || e === '2분기') metricsNumber = 2;
+      else if (e === '3분기') metricsNumber = 3;
+      else if (e === '4분기') metricsNumber = 4;
+
+      refetch();
+    },
+    [categorize],
+  );
+
+  useMountEffect(() => {
+    setCategorize(period === '반기' ? halfPeriodArr[0] : quarterPeriodArr[0]);
+    refetch();
+
+    return () => {};
+  }, [period]);
 
   useEffect(() => {
     const interval = setInterval(getTime, 1000);
@@ -119,164 +150,192 @@ const DashBoardPeriod = () => {
     return () => clearInterval(interval);
   }, []);
 
-  const handleChangeCategorize = (e: string) => {
-    setCategorize(e);
-
-    if (e === '상반기' || e === '1분기') metricsNumber = 1;
-    else if (e === '하반기' || e === '2분기') metricsNumber = 2;
-    else if (e === '3분기') metricsNumber = 3;
-    else if (e === '4분기') metricsNumber = 4;
-
-    refetch();
+  const handleScroll = (elem: any) => {
+    if (window.scrollY > elem + 36) {
+      periodRef.current?.classList.add('fixed');
+      // if (headerElem) headerElem.style.position = 'absolute';
+      // periodRef.current?.classList.add('fix');
+      // periodChildRef.current?.classList.add('fixed');
+    } else {
+      // if (headerElem) headerElem.style.position = 'fixed';
+      // document.getElementById('header').style.position = 'relative';
+      periodRef.current?.classList.remove('fixed');
+      // periodChildRef.current?.classList.remove('fixed');
+    }
   };
 
-  return (
-    <div className={styles.root}>
-      <div className={styles.periodContainer}>
-        {isMobile ? (
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setPeriodModal({ type: 'period', show: true });
-              }}
-            >
-              {period}
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                setPeriodModal({ type: 'quarter', show: true });
-              }}
-            >
-              {categorize}
-            </button>
-            <BottomSheep
-              title="기간"
-              onClose={() => setPeriodModal({ type: '', show: false })}
-              open={periodModal.show}
-            >
-              {(periodModal.type === 'period'
-                ? periodArr
-                : period === '반기'
-                ? halfPeriodArr
-                : quarterPeriodArr
-              )?.map((v, i) => (
-                <button
-                  type="button"
-                  key={i}
-                  className={cn('select-btn', {
-                    ['selected']:
-                      periodModal.type === 'period'
-                        ? period === v
-                        : categorize === v,
-                  })}
-                  onClick={() => {
-                    if (periodModal.type === 'period') setPeriod(v);
-                    else handleChangeCategorize(v);
+  useEffect(() => {
+    const elem = getOffsetTop(periodRef.current);
+    const timer = setInterval(() => {
+      window.addEventListener('scroll', () => handleScroll(elem), {
+        capture: true,
+      });
+    }, 100);
 
-                    setPeriodModal({ type: '', show: false });
-                  }}
-                >
-                  {v}
-                  {periodModal.type === 'period' && (
-                    <span className={styles.periodDescription}>
-                      {v === '분기' ? ' (3개월 단위)' : ' (6개월 단위)'}
-                    </span>
-                  )}
-                </button>
-              ))}
-            </BottomSheep>
-          </div>
-        ) : (
-          <div className={styles.periodCategory}>
-            <label>
-              <span className={styles.title}>기간</span>
-              <Select value={period} setValue={setPeriod} options={periodArr} />
-            </label>
-            <label>
-              <span className={styles.title}>분류</span>
-              <Select
-                value={categorize}
-                options={period === '반기' ? halfPeriodArr : quarterPeriodArr}
-                onChange={(e) => handleChangeCategorize(e)}
-              />
-            </label>
-          </div>
-        )}
-        {!isMobile && (
-          <div className={styles.periodOfTime}>
-            <button type="button" onClick={() => setTooltipOpen(!tooltipOpen)}>
+    return () => {
+      clearInterval(timer);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className={styles.root}>
+        <div className={styles.periodContainer} ref={periodRef}>
+          {isMobile ? (
+            <div className={styles.periodCategory}>
+              <button
+                type="button"
+                onClick={() => {
+                  setPeriodModal({ type: 'period', show: true });
+                }}
+              >
+                {period}
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  setPeriodModal({ type: 'quarter', show: true });
+                }}
+              >
+                {categorize}
+              </button>
+            </div>
+          ) : (
+            <div className={styles.periodCategory}>
+              <label>
+                <span className={styles.title}>기간</span>
+                <Select
+                  value={period}
+                  setValue={setPeriod}
+                  options={periodArr}
+                />
+              </label>
+              <label>
+                <span className={styles.title}>분류</span>
+                <Select
+                  value={categorize}
+                  options={period === '반기' ? halfPeriodArr : quarterPeriodArr}
+                  onChange={(e) => handleChangeCategorize(e)}
+                />
+              </label>
+            </div>
+          )}
+          {!isMobile && (
+            <div className={styles.periodOfTime}>
+              <button
+                type="button"
+                onClick={() => setTooltipOpen(!tooltipOpen)}
+              >
+                <AutoHeightImage
+                  src="/images/time-clock.png"
+                  alt="검색"
+                  width={20}
+                  height={20}
+                />
+                <ToolTip
+                  text={`목표 기간 : ${new Date().getFullYear()}.01.01 ~ ${new Date().getFullYear()}.12.31`}
+                  open={tooltipOpen}
+                  clasName={styles.tooltip}
+                />
+              </button>
+              {leftTimes}
+            </div>
+          )}
+        </div>
+        <ul className={styles.categoryContainer}>
+          <li className={styles.objectives}>
+            <div className={styles.icon}>
               <AutoHeightImage
-                src="/images/time-clock.png"
-                alt="검색"
-                width={20}
-                height={20}
+                src="/images/trophy.png"
+                alt="목표"
+                width={24}
+                height={24}
               />
-              <ToolTip
-                text={`목표 기간 : ${new Date().getFullYear()}.01.01 ~ ${new Date().getFullYear()}.12.31`}
-                open={tooltipOpen}
-                clasName={styles.tooltip}
+            </div>
+            <div>
+              <strong>{obejectStatus ? '달성완료' : '달성중'}</strong>
+              <p>목표</p>
+            </div>
+            <div className={styles.progress}>
+              <span style={{ width: `${obejectStatus ? 100 : 0}%` }} />
+            </div>
+          </li>
+          <li className={styles.keyResult}>
+            <div className={styles.icon}>
+              <AutoHeightImage
+                src="/images/flag.png"
+                alt="결과"
+                width={24}
+                height={24}
               />
-            </button>
-            {leftTimes}
-          </div>
-        )}
+            </div>
+            <div>
+              <strong>{keyResultPercent}%</strong>
+              <p>핵심 지표</p>
+            </div>
+            <div className={styles.progress}>
+              <span style={{ width: `${keyResultPercent}%` }} />
+            </div>
+          </li>
+          <li className={styles.initiatives}>
+            <div className={styles.icon}>
+              <AutoHeightImage
+                src="/images/todo.png"
+                alt="계획"
+                width={24}
+                height={24}
+              />
+            </div>
+            <div>
+              <strong>{initiativePercent}%</strong>
+              <p>주요 행동</p>
+            </div>
+            <div className={styles.progress}>
+              <span style={{ width: `${initiativePercent}%` }} />
+            </div>
+          </li>
+        </ul>
       </div>
-      <ul className={styles.categoryContainer}>
-        <li className={styles.objectives}>
-          <div className={styles.icon}>
-            <AutoHeightImage
-              src="/images/trophy.png"
-              alt="목표"
-              width={24}
-              height={24}
-            />
-          </div>
-          <div>
-            <strong>{obejectStatus ? '달성완료' : '달성중'}</strong>
-            <p>목표</p>
-          </div>
-          <div className={styles.progress}>
-            <span style={{ width: `${obejectStatus ? 100 : 0}%` }} />
-          </div>
-        </li>
-        <li className={styles.keyResult}>
-          <div className={styles.icon}>
-            <AutoHeightImage
-              src="/images/flag.png"
-              alt="결과"
-              width={24}
-              height={24}
-            />
-          </div>
-          <div>
-            <strong>{keyResultPercent}%</strong>
-            <p>핵심 지표</p>
-          </div>
-          <div className={styles.progress}>
-            <span style={{ width: `${keyResultPercent}%` }} />
-          </div>
-        </li>
-        <li className={styles.initiatives}>
-          <div className={styles.icon}>
-            <AutoHeightImage
-              src="/images/todo.png"
-              alt="계획"
-              width={24}
-              height={24}
-            />
-          </div>
-          <div>
-            <strong>{initiativePercent}%</strong>
-            <p>주요 행동</p>
-          </div>
-          <div className={styles.progress}>
-            <span style={{ width: `${initiativePercent}%` }} />
-          </div>
-        </li>
-      </ul>
-    </div>
+      {isMobile && (
+        <BottomSheep
+          title="기간"
+          onClose={() => setPeriodModal({ type: '', show: false })}
+          open={periodModal.show}
+        >
+          {(periodModal.type === 'period'
+            ? periodArr
+            : period === '반기'
+            ? halfPeriodArr
+            : quarterPeriodArr
+          )?.map((v, i) => (
+            <button
+              type="button"
+              key={i}
+              className={cn('select-btn', {
+                ['selected']:
+                  periodModal.type === 'period'
+                    ? period === v
+                    : categorize === v,
+              })}
+              onClick={() => {
+                if (periodModal.type === 'period') setPeriod(v);
+                else handleChangeCategorize(v);
+
+                setPeriodModal({ type: '', show: false });
+              }}
+            >
+              {v}
+              {periodModal.type === 'period' && (
+                <span className={styles.periodDescription}>
+                  {v === '분기' ? ' (3개월 단위)' : ' (6개월 단위)'}
+                </span>
+              )}
+            </button>
+          ))}
+        </BottomSheep>
+      )}
+    </>
   );
 };
 
