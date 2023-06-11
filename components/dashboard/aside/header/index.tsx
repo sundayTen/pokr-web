9a64379eea9a026ce879dd9c8fa92b396668d029
hojunin <dlsghwns@naver.com>
2023-06-11 18:52:26 +0900

    feat: 캘린더 기능 구현

--- components/dashboard/aside/header/index.tsx
+++ components/dashboard/aside/header/index.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.scss';
 import cn from 'classnames';
 import Image from 'next/image';
+import dayjs from 'dayjs';
 
-const Header = () => {
+interface AsideHeaderProps {
+  currentDate: dayjs.Dayjs;
+  onClickToday: () => void;
+  onClickPrev: () => void;
+  onClickNext: () => void;
+}
+
+const Header = ({
+  currentDate,
+  onClickNext,
+  onClickPrev,
+  onClickToday,
+}: AsideHeaderProps) => {
   return (
     <div className={styles.root}>
       <div className={styles.container}>
-        <h1>2023년 1월 1일</h1>
+        <h1>{currentDate.format('YYYY년 MM월 DD일')}</h1>
         <div className={styles.buttons}>
-          <button className={cn(styles.buttonContainer, styles.todayButton)}>
+          <button
+            className={cn(styles.buttonContainer, styles.todayButton)}
+            onClick={onClickToday}
+          >
             오늘
           </button>
 
           <div className={styles.buttons}>
-            <button className={cn(styles.buttonContainer, styles.left)}>
+            <button
+              className={cn(styles.buttonContainer, styles.left)}
+              onClick={onClickPrev}
+            >
               <Image
                 src={'/images/prev-arrow.png'}
                 width={24}
@@ -22,7 +41,10 @@ const Header = () => {
                 alt="지난 주 보기 아이콘"
               />
             </button>
-            <button className={cn(styles.buttonContainer, styles.right)}>
+            <button
+              className={cn(styles.buttonContainer, styles.right)}
+              onClick={onClickNext}
+            >
               <Image
                 src={'/images/next-arrow.png'}
                 width={24}
