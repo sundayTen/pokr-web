ae643c6fe8a777de8001aa5854692ad24babf475
hojunin <dlsghwns@naver.com>
2023-07-01 11:12:17 +0900

    fix: 로그인 - 하드코딩된 토큰 제거

--- store/user.ts
+++ store/user.ts
@@ -6,8 +6,7 @@ interface UserTokenStore {
 }
 
 const userStore = create<UserTokenStore>((set) => ({
-  userToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwibmFtZSI6bnVsbCwibmlja25hbWUiOiJcdWFjMWNcdWJjMWNcdWQ1NThcdWIyOTQgXHVjN2EwXHVjNzkwXHViOWFjIiwiZW1haWwiOm51bGwsImV4cCI6MTY4OTI0NzU1M30.Kc8vSb9vEEZIRlSdM3MJnstnwtTt8r0U9RYeZttsmtM',
+  userToken: null,
   changeUserToken: (data: string | null) => set({ userToken: data }),
 }));
 
