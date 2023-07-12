159dcb771b6dd966839475b7b58f0b1f6bb6d842
hojunin <dlsghwns@naver.com>
2023-07-12 10:08:26 +0900

    fix: SSR 빌드오류 수정

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import DashBoard from '@components/dashboard';
-import { fetchOkrYears } from '@api/okr';
-import { getObjectives } from '@api/objectives';
 
 const Page = async () => {
-  const years = await fetchOkrYears();
-  const objectiveList = await getObjectives(years?.[0]);
-
   return <DashBoard />;
 };
 
