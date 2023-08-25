04da9550c67c64dcedcd11ad534e0030113ae2cf
hojunin <dlsghwns@naver.com>
2023-08-25 17:06:48 +0900

    fix: 로그인 조건부 라우팅 롤백

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,7 @@
-'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import DashBoard from '@components/dashboard';
-import userStore from '@store/user';
-import { useRouter } from 'next/navigation';
 
 const Page = async () => {
-  const { isLogin } = userStore();
-  const { replace } = useRouter();
-
-  useEffect(() => {
-    if (!isLogin) {
-      replace('/');
-      return;
-    }
-  }, [isLogin]);
-
   return <DashBoard />;
 };
 
--- app/goal-management/page.tsx
+++ app/goal-management/page.tsx
@@ -1,18 +1,7 @@
-'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import GoalManagement from '@components/goal-management';
-import { useRouter } from 'next/navigation';
-import userStore from '@store/user';
 
 const Page = () => {
-  const { isLogin } = userStore();
-  const { replace } = useRouter();
-
-  useEffect(() => {
-    if (!isLogin) {
-      replace('/');
-    }
-  }, [isLogin]);
   return <GoalManagement />;
 };
 
